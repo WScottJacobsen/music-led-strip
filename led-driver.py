14811ae88c58d1d40224e5353cf721383b1881c8
WScottJacobsen <jacobsenscott07@gmail.com>
2017-09-06 15:59:44 -0500

    setup framework for accessing strip

--- led-driver.py
+++ led-driver.py
@@ -1,1 +1,9 @@
 # Main file to drive LEDs
+
+import time
+from dotstar import Adafruit_DotStar
+
+numpixels = 144 # Number of LEDs in strip
+
+# Here's how to control the strip from any two GPIO pins:
+strip     = Adafruit_DotStar(numpixels, 12000000)
