f423d27df698250c19b041db0ab7add48c06e4a2
BuildTools <unconfigured@null.spigotmc.org>
2017-09-06 18:53:55 -0500

    Wrote simple rainbow pattern for strip

--- led-driver.py
+++ led-driver.py
@@ -3,7 +3,23 @@
 import time
 from dotstar import Adafruit_DotStar
 
-numpixels = 144 # Number of LEDs in strip
+num_pixels = 288 # Number of LEDs in strip
 
-# Here's how to control the strip from any two GPIO pins:
-strip     = Adafruit_DotStar(numpixels, 12000000)
+strip = Adafruit_DotStar(numpixels, 12000000) #Initialize strip
+strip.begin()
+
+while True:
+    strip.show()
+    display_rainbow(0.3)
+
+#Rainbow Display
+def display_rainbow(frequency):
+    for x in range(0, num_pixels):
+        red   = math.sin(frequency * x) * 127 + 128
+        green = math.sin(frequency * x + 2 * math.pi / 3) * 127 + 128
+        blue  = math.sin(frequency * x + 4 * math.pi / 3) * 127 + 128
+        strip.setPixelColor(x, red, green, blue)
+
+def set_all_pixels(r, g, b):
+    for x in range(0, num_pixels):
+        strip.setPixelColor(x, r, g, b)
