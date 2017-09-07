ddfb1244e0de910cb80ab3744f82b30986f3e676
WScottJacobsen <jacobsenscott07@gmail.com>
2017-09-07 15:57:23 -0500

    Implemented some effects

--- led-driver.py
+++ led-driver.py
@@ -9,7 +9,7 @@ strip = Adafruit_DotStar(num_pixels, 12000000, order='bgr') # Initialize strip
 strip.begin()
 strip.setBrightness(10) # Save my eyes
 
-effect.set_strip(strip)
+effect.set_strip(strip, num_pixels) # Set up effects module
 while True:
     strip.show()
     effect.wander()
