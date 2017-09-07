f68bda42aa1984c8996bd24783afea01e18bcbb7
BuildTools <unconfigured@null.spigotmc.org>
2017-09-06 19:26:52 -0500

    Moved dotstar library into root directory so that led-driver has access to it

--- led-driver.py
+++ led-driver.py
@@ -7,19 +7,24 @@ num_pixels = 288 # Number of LEDs in strip
 
 strip = Adafruit_DotStar(numpixels, 12000000) #Initialize strip
 strip.begin()
-
-while True:
-    strip.show()
-    display_rainbow(0.3)
+strip.setBrightness(50)
 
 #Rainbow Display
-def display_rainbow(frequency):
+def display_rainbow(frequency, start = 0):
     for x in range(0, num_pixels):
-        red   = math.sin(frequency * x) * 127 + 128
-        green = math.sin(frequency * x + 2 * math.pi / 3) * 127 + 128
-        blue  = math.sin(frequency * x + 4 * math.pi / 3) * 127 + 128
+        #Uses three out of sync sin waves to have a smooth transition between colors
+        red   = int(math.sin(frequency * (x + start)) * 127 + 128)
+        green = int(math.sin(frequency * (x + start) + 2 * math.pi / 3) * 127 + 128)
+        blue  = int(math.sin(frequency * (x + start) + 4 * math.pi / 3) * 127 + 128)
         strip.setPixelColor(x, red, green, blue)
 
+#Sets all pixels on strip to same color
 def set_all_pixels(r, g, b):
     for x in range(0, num_pixels):
         strip.setPixelColor(x, r, g, b)
+
+start = 0
+while True:
+    strip.show()
+    display_rainbow(0.3, start)
+    start++ #Shifts rainbow down strip
