7e98650272861583eddd8b3144adddae34ae7e83
BuildTools <unconfigured@null.spigotmc.org>
2017-09-06 22:27:02 -0500

    Added and implemented solid rainbow effect

--- led-driver.py
+++ led-driver.py
@@ -6,27 +6,33 @@ from dotstar import Adafruit_DotStar
 
 num_pixels = 288 # Number of LEDs in strip
 
-strip = Adafruit_DotStar(num_pixels, 12000000) #Initialize strip
+strip = Adafruit_DotStar(num_pixels, 12000000, order='bgr') # Initialize strip
 strip.begin()
-strip.setBrightness(50)
+strip.setBrightness(10) # Save my eyes
 
-#Rainbow Display
-def display_rainbow(frequency, start = 0):
-    for x in range(0, num_pixels):
-        #Uses three out of sync sin waves to have a smooth transition between colors
-        red   = int(math.sin(frequency * (x + start)) * 127 + 128)
-        green = int(math.sin(frequency * (x + start) + 2 * math.pi / 3) * 127 + 128)
-        blue  = int(math.sin(frequency * (x + start) + 4 * math.pi / 3) * 127 + 128)
-        strip.setPixelColor(x, red, green, blue)
+# Rainbow Display
+def moving_rainbow(frequency, start = 0):
+    for i in range(0, num_pixels):
+        color = get_rainbow_color(i, start)
+        strip.setPixelColor(i, color[0], color[1], color[2])
 
-#Sets all pixels on strip to same color
+def get_rainbow_color(frequency, start, offset = 0):
+    # Uses three out of sync sin waves to have a smooth transition between colors
+    # frequency is how quickly it moves throught the rainbow, start is the position in the rainbow, offset is how much it is offset by
+    red   = int(math.sin(frequency * (start + offset)) * 127 + 128)
+    green = int(math.sin(frequency * (start + offset) + 2 * math.pi / 3) * 127 + 128)
+    blue  = int(math.sin(frequency * (start + offset) + 4 * math.pi / 3) * 127 + 128)
+    return [red, green, blue]
+
+# Sets all pixels on strip to same color
 def set_all_pixels(r, g, b):
-    for x in range(0, num_pixels):
-        strip.setPixelColor(x, r, g, b)
+    for i in range(0, num_pixels):
+        strip.setPixelColor(i, r, g, b)
 
 start = 0
 while True:
     strip.show()
-    display_rainbow(0.3, start)
-    start += 1 #Shifts rainbow down strip
-    time.sleep(1.0 / 120) # Pause 20 milliseconds (~50 fps)
+    set_all_pixels(get_rainbow_color(0.3, start))
+    start += 1 # Shifts rainbow down strip
+    start %= num_pixels
+    time.sleep(1.0 / 50) # Pause 20 milliseconds (~50 fps)
