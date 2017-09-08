41e0edbafc01f13a65b00d369295b556bf2ef90a
BuildTools <unconfigured@null.spigotmc.org>
2017-09-08 17:45:40 -0500

    test

--- effect.py
+++ effect.py
@@ -98,14 +98,8 @@ def wander_2(speed = 0.03):
     for i in range(0, num_pixels):
         curr_color = strip.getPixelColor(i)
         rgb = hex_to_rgb(curr_color)
-        r = rgb[0] / 255.0
-        g = rgb[1] / 255.0
-        b = rgb[2] / 255.0
         hsv = colorsys.rgb_to_hsv(r, g, b)
-        rgb = colorsys.hsv_to_rgb((hsv[0] + random.uniform(-1, 1) * speed) * 359, hsv[1], hsv[2])
-        r = rgb[0] * 255
-        g = rgb[1] * 255
-        b = rgb[2] * 255
+        rgb = colorsys.hsv_to_rgb((hsv[0] + random.uniform(-1, 1) * speed), hsv[1], hsv[2])
         if i == 1:
             print(hsv[0], hsv[1], hsv[2])
         strip.setPixelColor(i, int(r), int(g), int(b))
