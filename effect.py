bce19d95b691e1aa1908977089ccd5ce22698b4f
BuildTools <unconfigured@null.spigotmc.org>
2017-09-08 17:24:59 -0500

    test

--- effect.py
+++ effect.py
@@ -101,7 +101,7 @@ def wander_2(speed = 0.03):
         r = translate(rgb[0], 0, 255, 0, 1)
         g = translate(rgb[1], 0, 255, 0, 1)
         b = translate(rgb[2], 0, 255, 0, 1)
-        print(r, g, b)
+        print(random.uniform(-1, 1))
         hsv = colorsys.rgb_to_hsv(r, g, b)
         rgb = colorsys.hsv_to_rgb(hsv[0] + random.uniform(-1, 1) * speed, hsv[1], hsv[2])
         r = translate(rgb[0], 0, 1, 0, 255)
