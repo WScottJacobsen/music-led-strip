d1654f852bbb957fd900886b829c38ee1113c928
BuildTools <unconfigured@null.spigotmc.org>
2017-09-08 21:21:08 -0500

    made usa effect move

--- effect.py
+++ effect.py
@@ -91,6 +91,7 @@ def wander(speed = 0.3, start_color = None, index = 0, wave = True):
             time.sleep(1 / 120.0) # Give it a 'wave' effect
 
 def usa(speed = 1 / 90.0, frequency = 10):
+    global pos
     for i in range(0, num_pixels / frequency):
          for x in range(0, frequency):
              if i % 3 == 0:
@@ -99,7 +100,7 @@ def usa(speed = 1 / 90.0, frequency = 10):
                  color = rgb_to_hex(0, 0, 255)
              else:
                  color = rgb_to_hex(255, 255, 255)
-             strip.setPixelColor(i * frequency + x, color)
+             strip.setPixelColor((i * frequency + x + pos) % num_pixels, color)
 
 def all_random():
     for i in range(0, num_pixels):
@@ -115,7 +116,7 @@ def wander_2(speed = 0.01):
 
         strip.setPixelColor(i, int(rgb[0]), int(rgb[1]), int(rgb[2]))
 
-def breathe(speed = 0.05, max_brightness = 100):
+def breathe(speed = 0.01, max_brightness = 100):
     global breathe_pos
     strip.setBrightness(int(translate(math.sin(speed * breathe_pos), -1, 1, 5, max_brightness)))
     breathe_pos += 1
